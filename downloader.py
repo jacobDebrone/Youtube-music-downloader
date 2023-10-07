dc20e5e08a456e7bd7ef263a6c4e6df1788374bb
jacobDebrone <129507402+jacobDebrone@users.noreply.github.com>
2023-10-07 15:26:21 +0300

    downloader.py
    Signed-off-by: jacobDebrone <129507402+jacobDebrone@users.noreply.github.com>

--- downloader.py
+++ downloader.py
@@ -1,92 +1,188 @@
+#!/usr/bin/env python3
 import os
-import subprocess
+import yt_dlp
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-import yt_dlp
+import threading
+from tqdm import tqdm
+import lyricsgenius
 
-# Set up the API key
-api_key = 'your-api-key'  # Replace with your actual YouTube Data API key
+# Define constants
+VIDEO_EXTENSION = '.mp4'
+AUDIO_EXTENSION = '.mp3'
 
-# Create a YouTube Data API client
+# Set up the API keys (Replace with your actual YouTube Data API key and Lyrics Genius API token)
+api_key = 'YOUTUBE-API'
+genius_token = 'GENIUS-TOKEN'
+
+# Create clients for YouTube Data API and Lyrics Genius API
 youtube = build('youtube', 'v3', developerKey=api_key)
+genius = lyricsgenius.Genius(genius_token)
+
 
 def download_media(video_id, is_audio=False):
-    """Downloads a YouTube video or audio using yt-dlp.
-
-    Args:
-        video_id: The ID of the YouTube video to download.
-        is_audio: True if downloading audio, False for video.
-    """
+    """Downloads a YouTube video or audio using yt-dlp."""
     ydl_opts = {
         'format': 'bestaudio/best' if is_audio else 'best',
-        'outtmpl': f'%(title)s.{"mp3" if is_audio else "mp4"}',
+        'outtmpl': f'%(title)s.{AUDIO_EXTENSION if is_audio else VIDEO_EXTENSION}',
     }
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(f'https://www.youtube.com/watch?v={video_id}', download=True)
+            if 'entries' in info_dict:
+                info_dict = info_dict['entries'][0]
+            print(f'Downloaded: {info_dict["title"]}{AUDIO_EXTENSION if is_audio else VIDEO_EXTENSION}')
+    except yt_dlp.DownloadError as e:
+        print(f"Error: {str(e)}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([f'https://www.youtube.com/watch?v={video_id}'])
+
+def download_video(video_info):
+    """Downloads a video in a separate thread with a progress bar."""
+    video_id = video_info['videoId']
+    with tqdm(unit='B', unit_scale=True, leave=False, miniters=1, desc=f'Downloading Video: {video_info["title"]}  ',
+              ncols=100) as progress_bar:
+        def hook(data):
+            progress_bar.update(data['downloaded_bytes'])
+
+        ydl_opts = {
+            'format': 'best',
+            'progress_hooks': [hook],
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([f'https://www.youtube.com/watch?v={video_id}'])
+
+
+def download_audio(video_info):
+    """Downloads audio in a separate thread with a progress bar."""
+    video_id = video_info['videoId']
+    with tqdm(unit='B', unit_scale=True, leave=False, miniters=1, desc=f'Downloading Audio: {video_info["title"]}  ',
+              ncols=100) as progress_bar:
+        def hook(data):
+            progress_bar.update(data['downloaded_bytes'])
+
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'progress_hooks': [hook],
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([f'https://www.youtube.com/watch?v={video_id}'])
+
 
 def search_videos(search_query):
-    """Searches for YouTube videos using the YouTube Data API.
+    """Searches for YouTube videos using the YouTube Data API."""
+    try:
+        search_response = youtube.search().list(
+            q=search_query,
+            type='video',
+            part='snippet',
+            maxResults=15
+        ).execute()
+        video_results = []
+        for item in search_response['items']:
+            video_results.append({
+                'title': item['snippet']['title'],
+                'videoId': item['id']['videoId']
+            })
+        return video_results
+    except HttpError as e:
+        print(f"HTTP Error: {str(e)}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
-    Args:
-        search_query: The search query to use.
 
-    Returns:
-        A list of dictionaries containing video titles and video IDs.
-    """
+def download_lyrics(song_title, artist_name):
+    """Downloads song lyrics using the Lyrics Genius API."""
+    try:
+        song = genius.search_song(song_title, artist_name)
+        if song:
+            lyrics_file = f"{song_title} - {artist_name}.txt"
+            with open(lyrics_file, 'w', encoding='utf-8') as f:
+                f.write(song.lyrics)
+            print(f"Lyrics downloaded: {lyrics_file}")
+        else:
+            print("Lyrics not found. Please try a different search query.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
-    # Search for YouTube videos
-    search_response = youtube.search().list(
-        q=search_query,
-        type='video',
-        part='snippet',
-        maxResults=10  # You can adjust the number of results as needed
-    ).execute()
-
-    # Extract video titles and video IDs from the search response
-    video_results = []
-    for item in search_response['items']:
-        video_results.append({
-            'title': item['snippet']['title'],
-            'videoId': item['id']['videoId']
-        })
-
-    return video_results
 
 def main():
-    # Get the user's search query
-    search_query = input('Enter a search query: ')
-
-    # Search for YouTube videos
-    video_results = search_videos(search_query)
-
-    # Display the search results with titles
-    print('Search results:')
-    for index, video_info in enumerate(video_results):
-        print(f'{index}: {video_info["title"]}')
-
-    # Get the user's choice of video
-    video_index = int(input('Enter the index of the video you want to download: '))
-
-    if 0 <= video_index < len(video_results):
-        # Get the video ID of the selected video
-        video_id = video_results[video_index]['videoId']
-
-        # Ask the user if they want to download audio or video
-        choice = input('Download as (v)ideo or (a)udio? ')
-
-        if choice.lower() == 'v':
-            # Download the video
-            download_media(video_id)
-            print(f'Downloaded the video: {video_results[video_index]["title"]}.mp4')
+    print("Program made by Jacob Debrone")
+    while True:
+        # Ask the user for their choice
+        choice = input('What would you like to download? (v)ideo, (a)udio, or (l)yrics? (type "exit" to quit): ')
+        if choice.lower() == 'exit':
+            break
+        elif choice.lower() == 'v':
+            # Get the user's search query
+            search_query = input('Enter a search query: ')
+            # Search for YouTube videos
+            video_results = search_videos(search_query)
+            if not video_results:
+                print('No videos found. Please try a different search query.')
+                continue
+            # Display the search results with titles
+            print('Search results:')
+            for index, video_info in enumerate(video_results):
+                print(f'{index}: {video_info["title"]}')
+            # Get the user's choice of video
+            while True:
+                try:
+                    video_index = int(input('Enter the index of the video you want to download (type -1 to go back): '))
+                    if video_index == -1:
+                        break
+                    if 0 <= video_index < len(video_results):
+                        # Get the video info of the selected video
+                        selected_video = video_results[video_index]
+                        # Download the video
+                        download_video(selected_video)
+                        break
+                    else:
+                        print('Invalid video index. Please enter a valid index.')
+                except ValueError:
+                    print('Invalid input. Please enter a valid index.')
         elif choice.lower() == 'a':
-            # Download the audio
-            download_media(video_id, is_audio=True)
-            print(f'Downloaded the audio: {video_results[video_index]["title"]}.mp3')
+            # Get the user's search query
+            search_query = input('Enter a search query: ')
+            # Search for YouTube videos
+            video_results = search_videos(search_query)
+            if not video_results:
+                print('No videos found. Please try a different search query.')
+                continue
+            # Display the search results with titles
+            print('Search results:')
+            for index, video_info in enumerate(video_results):
+                print(f'{index}: {video_info["title"]}')
+            # Get the user's choice of video
+            while True:
+                try:
+                    video_index = int(input('Enter the index of the video you want to download (type -1 to go back): '))
+                    if video_index == -1:
+                        break
+                    if 0 <= video_index < len(video_results):
+                        # Get the video info of the selected video
+                        selected_video = video_results[video_index]
+                        # Download the audio
+                        download_audio(selected_video)
+                        break
+                    else:
+                        print('Invalid video index. Please enter a valid index.')
+                except ValueError:
+                    print('Invalid input. Please enter a valid index.')
+        elif choice.lower() == 'l':
+            # Get the song title and artist name
+            song_title = input('Enter the song title: ')
+            artist_name = input('Enter the artist name: ')
+            # Download song lyrics
+            try:
+                download_lyrics(song_title, artist_name)
+            except UnicodeEncodeError:
+                print("An error occurred while downloading lyrics. Please try a different search query.")
+                continue
         else:
-            print('Invalid choice. Exiting.')
-    else:
-        print('Invalid video index. Exiting.')
+            print('Invalid choice. Please enter "v", "a", or "l".')
+
 
 if __name__ == '__main__':
     main()
